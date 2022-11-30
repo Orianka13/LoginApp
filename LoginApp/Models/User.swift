d28a3b853ee51f0fdeea98621eb0a45084ee86be
Orianka13 <odobrogor@gmail.com>
2022-11-30 19:00:44 +0400

    Добавлена модель

---
+++ LoginApp/Models/User.swift
@@ -0,0 +1,44 @@
+//
+//  User.swift
+//  LoginApp
+//
+//  Created by Олеся Егорова on 30.11.2022.
+//
+
+
+struct User {
+    let userName: String
+    let password: String
+    let person: Person
+}
+
+struct Person {
+    let name: String
+    let title: [String]
+    let descriptions: [String]
+    let photos: [String]
+    let experience: String
+    let hobbies: [Hobby]
+}
+
+struct Hobby {
+    let hobby: String
+    let description: String
+    let photo: String
+}
+
+extension User {
+    static func getUser() -> User {
+        User(userName: "User", password: "123",
+             person: Person(name: "Olesia Egorova",
+                            title: ["About me", "Hometown", "New life"],
+                            descriptions: ["Hi! I am Olesia Egorova!",
+                                           "I was born in a small town Orsk. There is a beautifull nature (if you like steppe)",
+                                           "10 years i lived in St. Petersburg and i love it so much. But now i moved to Tbilisi. I am a little bit sad about it but i believe in the best feauture!"],
+                            photos: ["1", "2", "3"],
+                            experience: "I've been developing on iOS for over a year. Before that, I tried to learn Java - but, unfortunately, it didn’t work. In general, I had nothing to do with programming (except for my school passion for web design☺️). I am set up in this matter as seriously as possible - I want to make the development of mobile applications my profession. I see the goal and see no obstacles! 👍",
+                            hobbies: [Hobby(hobby: "DIY", description: "I like to make different things with my hands: scarves, hats, brooches, wreaths, jewelry, etc. This is my antistress.", photo: "knitting"),
+                                      Hobby(hobby: "Bicycling", description: "I have a small folding bike - I traveled all the forests of Vsevolozhsk on it 😁😁", photo: "bycicle"),
+                                      Hobby(hobby: "Walking", description: "Madly in love with walks, travel and adventure👍) I hope now there will be more of this!", photo: "walk")]))
+    }
+}

c066a63f696050ed62a1c713fe6a19997d1b68ab
Orianka13 <odobrogor@gmail.com>
2022-12-01 13:13:21 +0400

    Настроила prepareFor(segue)

--- LoginApp/ViewControllers/LoginViewController.swift
+++ LoginApp/ViewControllers/LoginViewController.swift
@@ -19,14 +19,6 @@ final class LoginViewController: UIViewController {
         view.endEditing(true)
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let tabBarVC = segue.destination as? UITabBarController {
-            if let welcomeVC = tabBarVC.viewControllers?.first as? WelcomeViewController {
-                welcomeVC.person = user.person
-            }
-        }
-    }
-    
     @IBAction func logInTapped() {
         if userNameTextField.text == user.userName, passwordTextField.text == user.password {
             performSegue(withIdentifier: "showWelcomeVC", sender: nil)
@@ -43,11 +35,6 @@ final class LoginViewController: UIViewController {
         : showAlert(title: "Oops!", message: "Your password is \(user.password)")
     }
     
-    @IBAction func unwindSegue(_ segue: UIStoryboardSegue) {
-        userNameTextField.text = ""
-        passwordTextField.text = ""
-    }
-    
     private func showAlert(title: String, message: String, textField: UITextField? = nil){
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let okAction = UIAlertAction(title: "Ok", style: .default){ _ in
@@ -55,5 +42,30 @@ final class LoginViewController: UIViewController {
         }
         alert.addAction(okAction)
         present(alert, animated: true)
+    }
+}
+
+//MARK: - Navigation
+extension LoginViewController {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let tabBarVC = segue.destination as? UITabBarController else { return }
+        guard let viewControllers = tabBarVC.viewControllers else { return }
+        
+        for viewController in viewControllers {
+            if let welcomeVC = viewController as? WelcomeViewController {
+                welcomeVC.person = user.person
+            } else if let navigationVC = viewController as? UINavigationController {
+                if let personVC = navigationVC.topViewController as? PersonViewController {
+                    personVC.person = user.person
+                } else if let experienceVC = navigationVC.topViewController as? ExperienceViewController {
+                    experienceVC.person = user.person
+                }
+            }
+        }
+    }
+    
+    @IBAction func unwindSegue(_ segue: UIStoryboardSegue) {
+        userNameTextField.text = ""
+        passwordTextField.text = ""
     }
 }
--- LoginApp/ViewControllers/PersonViewController.swift
+++ LoginApp/ViewControllers/PersonViewController.swift
@@ -8,22 +8,11 @@
 import UIKit
 
 class PersonViewController: UIViewController {
+    
+    var person: Person!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        title = person.name
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
